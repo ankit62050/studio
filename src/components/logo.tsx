845aae864fb0f46ce03deb91f633d5ab9cfa9eca
ANKIT Mishra <ankitmishraa29@gmail.com>
2025-09-15 18:54:53 +0000

    add the provided image as janconnect logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,12 +8,13 @@ export function Logo({ className }: { className?: string }) {
         className
       )}>
       <Image
-        src="https://storage.googleapis.com/studiostoragetesting/project-logos/JanConnect/JanConnect-logo-1x.png"
+        src="https://storage.googleapis.com/maker-studio-support-images-prod/user-prompt-images/21535971-e946-4e4f-b3b4-106c64639434.gif"
         alt="JanConnect Logo"
         width={140}
         height={40}
         className="object-contain"
         priority
+        unoptimized
       />
     </div>
   );
