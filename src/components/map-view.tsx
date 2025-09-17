d9612ca6281f5d21bf142e3d23cf3819ef87dee9
ANKIT Mishra <ankitmishraa29@gmail.com>
2025-09-17 13:07:17 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/components/map-view.tsx
+++ src/components/map-view.tsx
@@ -11,9 +11,9 @@ if (typeof window !== 'undefined') {
     // @ts-ignore
     delete L.Icon.Default.prototype._getIconUrl;
     L.Icon.Default.mergeOptions({
-        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default.src,
-        iconUrl: require('leaflet/dist/images/marker-icon.png').default.src,
-        shadowUrl: require('leaflet/dist/images/marker-shadow.png').default.src,
+        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+        iconUrl: require('leaflet/dist/images/marker-icon.png'),
+        shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
     });
 }
 
